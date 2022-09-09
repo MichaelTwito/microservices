48b8d9de779d4ce7c0477e403bd79b180d47f901
Michael <michael.twito@annatel.net>
2022-09-09 16:04:57 +0300

    optimization

--- prediction_manager/datasets/iris_dataset.py
+++ prediction_manager/datasets/iris_dataset.py
@@ -2,17 +2,20 @@ import torch
 import pandas as pd
 from torch.utils.data import Dataset
 
+def iris_mappings(): 
+    return {'Iris-setosa': 0,'Iris-versicolor': 1,'Iris-virginica': 2}
+
 class IrisDataset(Dataset):
     """Represents an iris dataset.
     """
 
-    def __init__(self, csv_file,transform=None):
+    def __init__(self, csv_file):
         csv_df = pd.read_csv(csv_file)
         self.length = len(csv_df.index)
         self.features = torch.Tensor(csv_df.iloc[:, 1:-1].values)
-        label_mapping = {'Iris-setosa': 0,'Iris-versicolor': 1,'Iris-virginica': 2}
-        self.labels = torch.Tensor(csv_df.iloc[:,-1].apply(lambda x: label_mapping[x])).long()
+        self.labels = torch.Tensor(csv_df.iloc[:,-1].apply(lambda x: iris_mappings()[x])).long()
         
+
     def __getitem__(self, index):
         item =  self.features[index]
         label = self.labels[index]
