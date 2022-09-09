f30442f3ae1ee2b07b789eaa0608a50268eea3b2
Michael <michael.twito@annatel.net>
2022-09-09 16:19:58 +0300

    script optimization

--- scripts/release_to_ecr.sh
+++ scripts/release_to_ecr.sh
@@ -8,10 +8,10 @@ report_failure() {
 
 for var in "$@"
 do
-    aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/i9g5h9s5
-    docker build -t $var ./$var
-    docker tag $var:latest public.ecr.aws/i9g5h9s5/$var:latest
-    docker push public.ecr.aws/i9g5h9s5/$var:latest
+    (aws ecr-public get-login-password --region us-east-1 | docker login --username AWS --password-stdin public.ecr.aws/i9g5h9s5 && \
+    docker build -t $var ./$var && \
+    docker tag $var:latest public.ecr.aws/i9g5h9s5/$var:latest && \
+    docker push public.ecr.aws/i9g5h9s5/$var:latest)&
 done
 
 trap 'report_failure $LINENO' ERR
